1ed2c20eca90ceac8211f0f5a8efa0029e35089d
Charles Kubicek <c.kubicek@gmail.com>
2015-01-29 10:08:51 +0000

    preparing to release version 0.2.2

--- project/PluginBuild.scala
+++ project/PluginBuild.scala
@@ -19,7 +19,7 @@ import Keys._
 object PluginBuild extends Build {
 
   val pluginName = "bobby"
-  val pluginVersion = "0.2.2-SNAPSHOT"
+  val pluginVersion = "0.2.2"
 
   lazy val root = Project(pluginName, base = file("."), settings = Seq(
     version := pluginVersion,
