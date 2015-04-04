f183a831ac959880687fdb29a9f2ae9ffd0bc0c8
Duncan Crawford <duncan@tandemconsulting.eu>
2015-04-04 23:30:45 +0100

    refactor the pomExtra setting to remove need to have a pom xml block

--- project/PluginBuild.scala
+++ project/PluginBuild.scala
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import com.typesafe.sbt.SbtGit.git
 import sbt._
 import Keys._
 
@@ -43,47 +45,59 @@ object PluginBuild extends Build {
     ),
     publishArtifact := true,
     publishArtifact in Test := false,
-    git.useGitDescribe := true,
-    git.versionProperty := "NONE",
-    git.gitDescribedVersion <<= git.gitDescribedVersion((v) => v.map(_.drop(1))),
     HeaderSettings()
-  ) ++ ArtefactDescription() ++ defaultSettings()
+  ) ++ Git() ++ ArtefactDescription() ++ defaultSettings()
   ).enablePlugins(AutomateHeaderPlugin, GitVersioning)
 }
 
+object Git {
+  def apply() = Seq(
+    git.useGitDescribe := true,
+    git.versionProperty := "NONE",
+    git.gitDescribedVersion <<= git.gitDescribedVersion((v) => v.map(_.drop(1)))
+  )
+}
 
 object ArtefactDescription {
 
+  import scala.xml.transform.{RewriteRule, RuleTransformer}
+  import scala.xml.{Node => XmlNode, NodeSeq => XmlNodeSeq, _}
+
   def apply() = Seq(
-      pomExtra := (<url>https://www.gov.uk/government/organisations/hm-revenue-customs</url>
-        <licenses>
-          <license>
-            <name>Apache 2</name>
-            <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
-          </license>
-        </licenses>
-        <scm>
-          <connection>scm:git@github.com:hmrc/sbt-bobby.git</connection>
-          <developerConnection>scm:git@github.com:hmrc/sbt-bobby.git</developerConnection>
-          <url>git@github.com:hmrc/sbt-bobby.git</url>
-        </scm>
-        <developers>
-          <developer>
-            <id>jakobgrunig</id>
-            <name>Jakob Grunig</name>
-            <url>http://www.equalexperts.com</url>
-          </developer>
-          <developer>
-            <id>charleskubicek</id>
-            <name>Charles Kubicek</name>
-            <url>http://www.equalexperts.com</url>
-          </developer>
-          <developer>
-            <id>duncancrawford</id>
-            <name>Duncan Crawford</name>
-            <url>http://www.equalexperts.com</url>
-          </developer>
-        </developers>)
+      homepage := Some(url("https://www.gov.uk/government/organisations/hm-revenue-customs")),
+      licenses += ("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0")),
+      scmInfo := Some(
+        ScmInfo(
+          url("https://github.com/hmrc/sbt-bobby"),
+          "git@github.com:hmrc/sbt-bobby.git"
+        )
+      ),
+      developers := List(
+          Developer("jakobgrunig", "Jakob Grunig", "jgrunig@equalexperts.com", url("http://www.equalexperts.com")),
+          Developer("charleskubicek", "Charles Kubicek", "ckubicek@equalexperts.com", url("http://www.equalexperts.com")),
+          Developer("duncancrawford", "Duncan Crawford", "dcrawford@equalexperts.com", url("http://www.equalexperts.com"))
+        ),
+
+      // workaround for sbt/sbt#1834
+      pomPostProcess := { (node: XmlNode) =>
+        new RuleTransformer(new RewriteRule {
+          override def transform(node: XmlNode): XmlNodeSeq = node match {
+            case e: Elem
+              if e.label == "developers" =>
+              <developers>
+                {developers.value.map { dev =>
+                <developer>
+                  <id>{dev.id}</id>
+                  <name>{dev.name}</name>
+                  <email>{dev.email}</email>
+                  <url>{dev.url}</url>
+                </developer>
+              }}
+              </developers>
+            case _ => node
+          }
+        }).transform(node).head
+      }
     )
 
 }
