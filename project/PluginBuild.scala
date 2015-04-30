925b2c50b9b4d1805a4c2604f49ae0e1ca21b7bb
Charles Kubicek <charles.kubicek@digital.hmrc.gov.uk>
2015-04-30 13:59:33 +0100

    leave in v again after some depenency problems

--- project/PluginBuild.scala
+++ project/PluginBuild.scala
@@ -42,8 +42,8 @@ object PluginBuild extends Build {
       ),
       ArtefactDescription(),
       git.useGitDescribe := true,
-      git.versionProperty := "NONE",
-      git.gitDescribedVersion <<= git.gitDescribedVersion((v) => v.map(_.drop(1)))
+      git.versionProperty := "NONE"//,
+      //git.gitDescribedVersion <<= git.gitDescribedVersion((v) => v.map(_.drop(1)))
     )
 }
 
