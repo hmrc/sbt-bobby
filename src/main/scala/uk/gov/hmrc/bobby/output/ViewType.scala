886875c05ed86c2f63e49f3f946c10165f817d4a
Tim Bennett <2475478+bennetimo@users.noreply.github.com>
2020-02-06 15:50:28 +0000

    BDOG-554 refactor field names

--- src/main/scala/uk/gov/hmrc/bobby/output/ViewType.scala
+++ src/main/scala/uk/gov/hmrc/bobby/output/ViewType.scala
@@ -36,16 +36,16 @@ object DefaultRendering {
       case _ => " L"                  //local
     })
 
-    val levelS: Str = messageColour(m)(m.level.name)
-    val dependencyS: Str = {
+    val levelStr: Str = messageColour(m)(m.level.name)
+    val dependencyStr: Str = {
       val dep = s"${m.checked.moduleID.moduleName}$prefix"
       if(m.isPlugin) Color.Magenta(dep) else if (m.isLocal) Color.Blue(dep) else Str(dep)
     }
-    val viaS: Str = Str(m.dependencyChain.lastOption.map(_.moduleName).getOrElse(""))
-    val yourVersionS: Str = Str(m.checked.moduleID.revision)
-    val outlawedRangeS: Str = Str(m.checked.result.rule.map(_.range.toString()).getOrElse("-"))
-    val effectiveDateS: Str = Str(m.effectiveDate.map(_.toString).getOrElse("-"))
-    val reasonS: Str = Str(m.deprecationReason.map(_.toString).getOrElse("-"))
+    val viaStr: Str = Str(m.dependencyChain.lastOption.map(_.moduleName).getOrElse(""))
+    val yourVersionStr: Str = Str(m.checked.moduleID.revision)
+    val outlawedRangeStr: Str = Str(m.checked.result.rule.map(_.range.toString()).getOrElse("-"))
+    val effectiveDateStr: Str = Str(m.effectiveDate.map(_.toString).getOrElse("-"))
+    val reasonStr: Str = Str(m.deprecationReason.map(_.toString).getOrElse("-"))
   }
 }
 
@@ -54,13 +54,13 @@ case object Flat extends ViewType {
   override def headerNames: Seq[String] = Seq("Level", "Dependency", "Via", "Your Version", "Outlawed Range", "Effective From", "Reason")
 
   override def renderMessage(m: Message): Seq[Str] = Seq(
-    m.levelS,
-    m.dependencyS,
-    m.viaS,
-    m.yourVersionS,
-    m.outlawedRangeS,
-    m.effectiveDateS,
-    m.reasonS
+    m.levelStr,
+    m.dependencyStr,
+    m.viaStr,
+    m.yourVersionStr,
+    m.outlawedRangeStr,
+    m.effectiveDateStr,
+    m.reasonStr
   )
 
 }
@@ -69,11 +69,11 @@ case object Nested extends ViewType {
   override def headerNames: Seq[String] = Seq("Level", "Dependency", "Your Version", "Outlawed Range", "Effective From")
 
   override def renderMessage(m: Message): Seq[Str] = Seq(
-    messageColour(m)(m.levelS),
-    if(m.isLocal || m.isPlugin) m.dependencyS else Str(s" => ${m.dependencyS}"),
-    m.yourVersionS,
-    m.outlawedRangeS,
-    m.effectiveDateS
+    messageColour(m)(m.levelStr),
+    if(m.isLocal || m.isPlugin) m.dependencyStr else Str(s" => ${m.dependencyStr}"),
+    m.yourVersionStr,
+    m.outlawedRangeStr,
+    m.effectiveDateStr
   )
 }
 
@@ -81,12 +81,12 @@ case object Compact extends ViewType {
   override def headerNames: Seq[String] = Seq("Level", "Dependency", "Type", "Your Version", "Outlawed Range", "Effective From")
 
   override def renderMessage(m: Message): Seq[Str] = Seq(
-    m.levelS,
-    m.dependencyS,
-    m.viaS,
-    m.yourVersionS,
-    m.outlawedRangeS,
-    m.effectiveDateS
+    m.levelStr,
+    m.dependencyStr,
+    m.viaStr,
+    m.yourVersionStr,
+    m.outlawedRangeStr,
+    m.effectiveDateStr
   )
 }
 
