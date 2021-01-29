3d6e17b886044e81eb9a731f8fe9c979eb4326fd
Nigel Perkins <nigelhp@users.noreply.github.com>
2021-01-29 14:42:30 +0000

    BDOG-1021: remove unused functions

--- src/test/scala/uk/gov/hmrc/bobby/BobbySpec.scala
+++ src/test/scala/uk/gov/hmrc/bobby/BobbySpec.scala
@@ -16,37 +16,15 @@
 
 package uk.gov.hmrc.bobby
 
-import java.time.LocalDate
-
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import sbt.ModuleID
 import uk.gov.hmrc.bobby.domain.MessageBuilder._
 import uk.gov.hmrc.bobby.domain.MessageLevels.{ERROR, INFO, WARN}
 import uk.gov.hmrc.bobby.domain._
 
+import java.time.LocalDate
+
 class BobbySpec extends AnyFlatSpec with Matchers {
-
-  it should "compact dependencies by only including them once if declared in multiple configurations" in {
-    val mods = Seq(
-      ModuleID("uk.gov.hmrc", "auth", "3.2.0").withConfigurations(Some("test")),
-      ModuleID("uk.gov.hmrc", "auth", "3.2.0"))
-
-    Bobby.compactDependencies(mods).size shouldBe 1
-  }
-
-  it should "prepare dependencies by removing any on a blacklist" in {
-    val auth = ModuleID("uk.gov.hmrc", "auth", "3.2.0")
-
-    val mods = Seq(
-      auth,
-      ModuleID("com.typesafe.play", "play-ws", "6.2.0")
-    )
-
-    val blacklisted: Set[String] = Set("com.typesafe.play")
-
-    Bobby.filterDependencies(mods, blacklisted) shouldBe Seq(auth)
-  }
 
   it should "order messages correctly" in {
     val rule = BobbyRule(Dependency("uk.gov.hmrc", "auth"), VersionRange("(,3.0.0]"), "testing", LocalDate.now())
